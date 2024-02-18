d692509fa4599331b6f6fcac7f84904f287b0841
dendefo <daniek332@gmail.com>
2024-02-19 00:16:11 +0200

    cleanUp

--- Program.cs
+++ Program.cs
@@ -5,15 +5,6 @@
 
         static void Main(string[] args)
         {
-
-            Triangle tri = new(5,5,2.587f,5,60);
-            Console.WriteLine(tri);
-            Square square = new(5, 5, 5);
-            Console.WriteLine(square);
-            Circle circle = new(5, 5, 5);
-            Console.WriteLine(circle);
-
-
         }
     }
 }
